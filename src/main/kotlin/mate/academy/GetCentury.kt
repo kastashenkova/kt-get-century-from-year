dc52dbb3a6a915539198dc828013a1a705f45b8b
kastashenkova <astashenkova.katya@gmail.com>
2025-12-18 23:38:06 +0200

    compute century

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,8 +1,5 @@
 package mate.academy
 
-const val YEARS_IN_CENTURY = 100
-const val ONE_YEAR = 1
-
 fun getCentury(year: Int) : Int {
-    return (year - ONE_YEAR) / YEARS_IN_CENTURY + ONE_YEAR
+    return 0
 }
